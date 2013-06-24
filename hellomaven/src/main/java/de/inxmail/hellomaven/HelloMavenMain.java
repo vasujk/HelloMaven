ce7119d0414010de1ea4aa84f2b10c88f1edbaa8
Vasumathi Jayakumar <vasujk80@yahoo.co.in>
2013-06-24 15:17:01 +0200

    My first commit !!!

---
+++ hellomaven/src/main/java/de/inxmail/hellomaven/HelloMavenMain.java
@@ -0,0 +1,21 @@
+package de.inxmail.hellomaven;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+public class HelloMavenMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		Logger logger = LoggerFactory.getLogger(HelloMavenMain.class);
+		logger.info("Hello Maven!");
+		// TODO Auto-generated method stub
+// System.out.println ("Hello Maven");
+	}
+
+}
